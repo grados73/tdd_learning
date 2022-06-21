a911c5e6cd89e0a584eafb887f99a701f36551f3
grad73 <gradowski.kam@gmail.com>
2022-06-21 12:13:33 +0200

    An empty project file with the requirements, added.

---
+++ bowling_game_counter.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdio.h>
+
+void roll(int);     // is called each time the player rolls a ball. The argument is the number of pins knocked down
+int score();        // returns the total score for that game
+
+int main(void)
+{
+
+    return 0;
+}
