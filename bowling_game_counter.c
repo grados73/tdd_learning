bfbd47b0ce6edf706c7f44a8e2d8a5ae47d3842f
grados73 <gradowski.kam@gmail.com>
2022-11-19 14:28:07 +0100

    Add score repaired.

--- bowling_game_counter.c
+++ bowling_game_counter.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_NUMBER_OF_FRAME 10
 #define ASCII_TO_UINT_CONST 48
@@ -11,13 +12,20 @@ typedef uint32_t number_of_frame_t;
 typedef uint32_t number_of_rolls_t;
 typedef uint32_t first_rolls_pins_uint_t;
 typedef uint32_t second_rolls_pins_uint_t;
+typedef uint32_t first_roll_number_t;
+typedef bool is_strike_t;
+typedef bool is_spare_t;
 
 score_t number_of_points = 0;
-number_of_frame_t number_of_frame = 1; 
-number_of_rolls_t number_of_rolls = 0;
+number_of_frame_t number_of_frame = 1;
+number_of_rolls_t number_of_rolls = 1;
+first_roll_number_t first_roll_number = 0;
+is_strike_t is_strike = false;
+is_strike_t is_strike_again = false;
+is_spare_t is_spare = false;
 
 char first_rolls_pins_char[5];
-char second_rolls_pins_char[5];  
+char second_rolls_pins_char[5];
 
 first_rolls_pins_uint_t first_rolls_pins_uint = 0;
 second_rolls_pins_uint_t second_rolls_pins_uint = 0;
@@ -35,7 +43,7 @@ int main(void)
     {
         uint32_t temp_number_knoceked = 0;
         // first rolls
-        
+
         printf("Give number of pins knocked down in %d frame and %d rolls: ", number_of_frame, number_of_rolls);
         fgets(first_rolls_pins_char, 5, stdin);
 
@@ -59,18 +67,18 @@ int main(void)
             printf("-Saved number of pins knocked down in first rolls == %d == \r\n", first_rolls_pins_uint);
         }
 
-        roll(first_rolls_pins_uint);  
+        roll(first_rolls_pins_uint);
 
         number_of_rolls++;
 
         if(first_rolls_pins_uint != STRIKE_VALUE) // if wasn`t strike in first roll
         {
-            printf("Give number of pins knocked down in %d frame and %d rolls: ", number_of_frame, number_of_rolls);
+            printf("\r\nGive number of pins knocked down in %d frame and %d rolls: ", number_of_frame, number_of_rolls);
             fgets( second_rolls_pins_char, 5, stdin);
             printf("%s", second_rolls_pins_char);
             if(second_rolls_pins_char[0] == '1')
             {
-                if (strncmp(second_rolls_pins_char, "10", 2) == 0) // 10 - STRIKE
+                if (strncmp(second_rolls_pins_char, "10", 2) == 0) // 10 - SPARE
                 {
                     second_rolls_pins_uint = 10;
                 }
@@ -88,25 +96,78 @@ int main(void)
 
             if((first_rolls_pins_uint + second_rolls_pins_uint) >= 10) printf(" - SPARE!\r\n\n");
             else printf("\r\n\n");
-            
-            number_of_rolls--;
-            number_of_frame++;
+
 
             roll(second_rolls_pins_uint);
         }
 
-        // second rolls
+        number_of_rolls--;
+        number_of_frame++;
 
-       
+        // second rolls
+    printf("Total score after %d frame: %d\r\n============================================================\r\n\n",  number_of_frame-1, score());
+
     }
     printf("\r\nTotal score of the game : !!! %d !!!",  score());
     return 0;
-    
+
 }
 
 void roll(int number_of_pins_in_one_roll)
 {
-    number_of_points = number_of_points + number_of_pins_in_one_roll;
+    if(number_of_rolls == 1)  // FIRST ROLL
+    {
+        if(number_of_pins_in_one_roll == 10)  // STRIKE
+        {
+            if(is_strike)
+            {
+                number_of_points = number_of_points + 2 * number_of_pins_in_one_roll;
+            }
+            else if(is_spare)
+            {
+                number_of_points = number_of_points + 2 * number_of_pins_in_one_roll;
+                is_spare = false;
+            }
+            else number_of_points = number_of_points + number_of_pins_in_one_roll;
+            is_strike_again = true;
+            is_strike = true;
+        }
+        else
+        {
+            first_roll_number = number_of_pins_in_one_roll;
+            if(is_strike)
+            {
+                number_of_points = number_of_points + 2 * number_of_pins_in_one_roll;
+            }
+            else if(is_spare)
+            {
+                number_of_points = number_of_points + 2 * number_of_pins_in_one_roll;
+                is_spare = false;
+            }
+            else number_of_points = number_of_points + number_of_pins_in_one_roll;
+            is_strike_again = false;
+        }
+    }
+
+    else    // SECOND ROLL
+    {
+        if((number_of_pins_in_one_roll + first_roll_number) >= 10)
+        {
+            is_spare = true;
+        }
+        if(is_strike)
+        {
+            number_of_points = number_of_points + 2 * number_of_pins_in_one_roll;
+            if(!is_strike_again) is_strike = false;
+        }
+        else number_of_points = number_of_points + number_of_pins_in_one_roll;
+    }
+
+
+
+
+
+
 }
 
 int score()
